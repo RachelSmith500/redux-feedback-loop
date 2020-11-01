ae5213039b956390e2b790c78532f5e2988e9b10
RachelSmith500 <rachelsmith9721@gmail.com>
2020-11-01 16:06:47 -0600

    Review component complete

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -3,7 +3,26 @@ import { connect } from 'react-redux';
 import axios from 'axios';
 
 class Review extends Component {
-
+    sendFeedback = () => {
+        axios.post('/api/feedback', this.props.reduxState.howWeDoingReducer)
+            .then(response => {
+                this.props.history.push('/completed');
+            }).catch(error => {
+                console.log('error in POST', error)
+            })
+    }
+    render (){
+        return(
+            <>
+            <div>Review Your Feedback:</div>
+            <div>Feelings:{this.props.reduxState.howWeDoingReducer.feel}</div>
+            <div>Understanding:{this.props.reduxState.howWeDoingReducer.understanding}</div>
+            <div>Support:{this.props.reduxState.howWeDoingReducer.support}</div>
+            <div>Comments:{this.props.reduxState.howWeDoingReducer.comments}</div>
+            <button onClick={this.sendFeedback}>Submit</button>
+            </>
+        )
+    }
 }
 
 const mapStateToProps = (reduxState) => { 
